9146871f34bc8a74e5ec1c3ddba786b39e5dc16a
Sajid-Zubair <sajid10zubair@gmail.com>
2025-06-29 13:55:50 -0400

    #

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -7,8 +7,8 @@ import SubjectFilter from '@/components/ui/SubjectFilter';
 
 const CompanionsLibrary = async ({ searchParams}: SearchParams) => {
   const filters = await searchParams;
-  const subject = filters.subject || '';
-  const topic = filters.topic || '';
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
 
 
   const companions = await getAllCompanions({ subject, topic })
