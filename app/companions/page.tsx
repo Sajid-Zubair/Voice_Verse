c5ce1cdd6564bc660776023fe876c19c09c50331
Sajid-Zubair <sajid10zubair@gmail.com>
2025-06-29 13:30:52 -0400

    #

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -16,7 +16,7 @@ const CompanionsLibrary = async ({ searchParams}: SearchParams) => {
   return(
     <main>
       <section className='flex justify-between gap-4 max-sm:flex-col'>
-        <h1>Companion Libray</h1>
+        <h1>Companion Library</h1>
         <div className='flex gap-4'>
           <SearchInput/>
           <SubjectFilter/>
